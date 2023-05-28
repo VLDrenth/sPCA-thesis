264629d6bc84e784ce25ebe4e6b44d33837604c1
vdrenth <v.l.drenth@student.tudelft.nl>
2023-05-28 15:05:15 +0200

    Integrated initial autoencoder model forecasts

--- src/helpers/autoencoder.py
+++ src/helpers/autoencoder.py
@@ -1,16 +1,18 @@
 import torch.nn as nn
 import torch
-from functions import get_data
+from src.helpers.functions import get_data
 
 class Autoencoder(nn.Module):
 
-    def __init__(self, hidden_dim=5, input_dim=123, layer_dims = [64, 32, 16], activation=nn.ReLU):
+    def __init__(self, hidden_dim=10, input_dim=123, layer_dims = [64, 32, 16], activation=nn.ReLU):
         super(Autoencoder, self).__init__()
         layer_dims = [input_dim] + layer_dims + [hidden_dim]
 
         self.encoder = create_sequential(layer_dims, activation=activation)
         self.decoder = create_sequential(layer_dims[::-1], activation=activation)
+
         self.init_weights()
+
         self.loss_criterion = nn.MSELoss()
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -26,6 +28,43 @@ class Autoencoder(nn.Module):
         output = self.decoder(hidden)
         return output
     
+    def encode(self, input):
+        # Not training the model so no need to compute gradients
+        with torch.no_grad():
+            # Ensure input is tensor and not numpy array
+            input = torch.from_numpy(input).float()
+            output = self.encoder(input)
+            
+            # Return numpy array
+            return output.cpu().detach().numpy()
+        
+    def train(self, X, lr=1e-3, num_epochs=100, batch_size=32):
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        num_epochs = num_epochs
+        batch_size = batch_size
+
+        # Convert to torch tensor
+        X = torch.from_numpy(X).float()
+
+        for epoch in range(num_epochs):
+            for i in range(0, X.shape[0], batch_size):
+                # Get batch
+                batch = X[i:i+batch_size]
+                batch = batch.to(self.device)
+
+                # Forward pass
+                output = self(batch)
+                loss = self.loss_criterion(output, batch)
+
+                # Backward pass
+                optimizer.zero_grad()
+                loss.backward()
+                
+                # Update
+                optimizer.step()
+            #print(f"Epoch: {epoch}, Loss: {loss.item()}")
+
+    
 def create_sequential(layer_dims, activation):
     layers = []
     for i in range(0, len(layer_dims) - 1):
@@ -34,37 +73,21 @@ def create_sequential(layer_dims, activation):
             layers.append(activation())
     return nn.Sequential(*layers)
     
-def train(X, lr, num_epochs=100, batch_size=32):
-    model = Autoencoder()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    num_epochs = num_epochs
-    batch_size = batch_size
-
-    # Convert to torch tensor
-    X = torch.from_numpy(X).float()
-
-    for epoch in range(num_epochs):
-        for i in range(0, X.shape[0], batch_size):
-            # Get batch
-            batch = X[i:i+batch_size]
-            batch = batch.to(model.device)
-
-            # Forward pass
-            output = model(batch)
-            loss = model.loss_criterion(output, batch)
-
-            # Backward pass
-            optimizer.zero_grad()
-            loss.backward()
-            
-            # Update
-            optimizer.step()
-        print(f"Epoch: {epoch}, Loss: {loss.item()}")
-    return model
 
 
 if __name__ == "__main__":
     X = get_data()['data'].values
-    model = train(X, lr=0.0005, num_epochs=100, batch_size=32)
+    X_train = X[:360]
+    X_test = X[360:]
+    model = Autoencoder(hidden_dim=10, input_dim=123, layer_dims = [64, 32, 16], activation=nn.ReLU)
+    model.train(X_train, lr=0.0005, num_epochs=200, batch_size=32)
+
+    # Get the latent representation of the data for the test set
+    X_test = torch.from_numpy(X_test).float()
+    X_test = X_test.to(model.device)
+    latent = model.encode(X_test).cpu().detach().numpy()
+
+
+
 
 
