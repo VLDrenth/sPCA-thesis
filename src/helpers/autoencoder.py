f7432b3088d33726c62d128843dbf876c19dac6a
vdrenth <v.l.drenth@student.tudelft.nl>
2023-05-30 19:29:39 +0200

    Added batchnorm and conv layers

--- src/helpers/autoencoder.py
+++ src/helpers/autoencoder.py
@@ -2,14 +2,64 @@ import torch.nn as nn
 import torch
 from src.helpers.functions import get_data
 
+
+class Encoder(nn.Module):
+    
+    def __init__(self, layer_dims, activation=nn.ReLU):
+        super().__init__()
+        
+        # Create fully connected layers for encoder
+        layers = create_layers(layer_dims, activation)
+        
+        # Add 1-D convolutional layer to capture temporal dependencies
+        layers.insert(0, nn.Conv1d(1, 1, 3, padding=1))
+        layers.insert(1, activation())
+        layers.insert(2, nn.BatchNorm1d(1))
+        layers.insert(3, nn.Conv1d(1, 1, 3, padding=1))
+        layers.insert(4, activation())
+        layers.insert(5, nn.BatchNorm1d(1))
+
+        # Create sequential model
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, input):
+        output = self.model(input)
+        return output
+
+
+class Decoder(nn.Module):
+    
+    def __init__(self, layer_dims, activation=nn.ReLU):
+        super().__init__()
+
+        # Fully connected part is the same as encoder but in reverse
+        layer_dims = layer_dims[::-1]
+        layers = create_layers(layer_dims, activation)
+
+        # Add 1-D convolutional layer at the end to capture temporal dependencies
+        layers.append(nn.ConvTranspose1d(1, 1, 3, padding=1))
+        layers.append(activation())
+        layers.append(nn.BatchNorm1d(1))
+        layers.append(nn.ConvTranspose1d(1, 1, 3, padding=1))
+        layers.append(activation())
+        layers.append(nn.BatchNorm1d(1))
+
+        # Create sequential model
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, input):
+        output = self.model(input)
+
+        return output
+
 class Autoencoder(nn.Module):
 
     def __init__(self, hidden_dim=10, input_dim=123, layer_dims = [64, 32, 16], activation=nn.ReLU):
         super(Autoencoder, self).__init__()
         layer_dims = [input_dim] + layer_dims + [hidden_dim]
 
-        self.encoder = create_sequential(layer_dims, activation=activation)
-        self.decoder = create_sequential(layer_dims[::-1], activation=activation)
+        self.encoder = Encoder(layer_dims, activation=activation)
+        self.decoder = Decoder(layer_dims, activation=activation)
 
         self.init_weights()
 
@@ -24,16 +74,25 @@ class Autoencoder(nn.Module):
                 nn.init.xavier_normal_(m.weight)
     
     def forward(self, input):
+        # Add Gaussian noise to input
+        input = input + torch.randn(input.shape).to(self.device) * 0.3
+
+        # Take input and reshape it so that it has 1 channel
         hidden = self.encoder(input)
         output = self.decoder(hidden)
         return output
     
     def encode(self, input):
+        input = input.reshape(input.shape[0], 1, input.shape[1])
+
         # Not training the model so no need to compute gradients
         with torch.no_grad():
             # Ensure input is tensor and not numpy array
             input = torch.from_numpy(input).float()
             output = self.encoder(input)
+
+            # Resahpe output to be 2D
+            output = output.reshape(output.shape[0], output.shape[2])
             
             # Return numpy array
             return output.cpu().detach().numpy()
@@ -52,6 +111,9 @@ class Autoencoder(nn.Module):
                 batch = X[i:i+batch_size]
                 batch = batch.to(self.device)
 
+                batch = batch.reshape(batch.shape[0], 1, batch.shape[1])
+
+
                 # Forward pass
                 output = self(batch)
                 loss = self.loss_criterion(output, batch)
@@ -65,16 +127,15 @@ class Autoencoder(nn.Module):
             #print(f"Epoch: {epoch}, Loss: {loss.item()}")
 
     
-def create_sequential(layer_dims, activation):
+def create_layers(layer_dims, activation):
     layers = []
     for i in range(0, len(layer_dims) - 1):
         layers.append(nn.Linear(layer_dims[i], layer_dims[i+1]))
         if i != len(layer_dims) - 2:
             layers.append(activation())
-    return nn.Sequential(*layers)
+            
+    return layers
     
-
-
 if __name__ == "__main__":
     X = get_data()['data'].values
     X_train = X[:360]
